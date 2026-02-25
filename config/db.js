3565bcd81557b67cb099adc6799071698bcbf0db
Loheshkanna55 <loheshkanna55@gmail.com>
2026-02-25 20:19:52 +0530

    Updated db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://Lohesh:<password>@cluster0-shard-00-00.n992qh5.mongodb.net:27017,cluster0-shard-00-01.n992qh5.mongodb.net:27017,cluster0-shard-00-02.n992qh5.mongodb.net:27017/urban_issues?ssl=true&replicaSet=atlas-xxxxx-shard-0&authSource=admin&retryWrites=true&w=majority');
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://Lohesh:Loki2004@cluster0-shard-00-00.n992qh5.mongodb.net:27017,cluster0-shard-00-01.n992qh5.mongodb.net:27017,cluster0-shard-00-02.n992qh5.mongodb.net:27017/urban_issues?ssl=true&replicaSet=atlas-xxxxx-shard-0&authSource=admin&retryWrites=true&w=majority');
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('MongoDB Connection Error:', err.message);
