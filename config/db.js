0522e245d734ef6dd5d189e3120c678fcf843103
Loheshkanna55 <loheshkanna55@gmail.com>
2026-02-25 20:03:32 +0530

    Updated db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://Lohesh:Loki2004@cluster0.n992qh5.mongodb.net/urban_issues?retryWrites=true&w=majority');
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://Lohesh:Loki2004@cluster0.n992qh5.mongodb.net/urban_issues');
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('MongoDB Connection Error:', err.message);
